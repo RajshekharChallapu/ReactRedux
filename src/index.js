e9e7f592dc3381809e9fb856365df64aab286e80
CR <rajshekhar@protonmail.com>
2017-12-26 00:17:50 -0600

    created react componenet

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// ES6....creating new component...
+const App = function() {
+    return <div > Hi < /div>;
+};
+
+ReactDOM.render( < App / > , document.querySelector('.container'));
